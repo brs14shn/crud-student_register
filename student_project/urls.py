a1c278ca5f1b909914f9e3c4eb5b842885beb157
brs14shn <brsshn292929@gmail.com>
2022-09-01 17:55:27 +0300

    updated

--- student_project/urls.py
+++ student_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("",include("student_register.urls"))
+    path("",include("student_register.urls")),
+   
 ]
