4a37bfc27f6f075b4156eeee135a3d1169088825
brs14shn <brsshn292929@gmail.com>
2022-09-07 20:45:24 +0300

    allowed

--- student_project/settings.py
+++ student_project/settings.py
@@ -24,9 +24,9 @@ from decouple import config
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
